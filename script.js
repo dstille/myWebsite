59bd6a159dfc9c51479ddf25c08e207da86ba727
dstille <dancstille@gmail.com>
2024-03-25 13:43:41 -0500

    prelim array implementation

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+function getCombosOf2(elements) {
+    out = [];
+    for (i=0; i<length(elements); i++) {
+        for (j=i+1; j<length(elements); j++) {
+            out.push(elements[i], elements[j]);
+        }
+    }
+    return out;
+}
+
+function addElem2Each(elem, sets) {
+    array.forEach(element => {
+        
+    });
+}
+
+
+
+
+
+
+
+
+
 document.getElementById("mathBox").innerHTML = "combination results go here!"
